60b9c68ac2116f994d5298b704218e3b36cdbc76
Muhammad-Talha-S <talhasheikh878@gmail.com>
2025-01-30 14:25:55 +0100

    modify data

---
+++ source/MySEProject/AnomalyDetectionTeamSynergy/AnomalyDetection.cs
@@ -0,0 +1,19 @@
+﻿namespace AnomalyDetectionTeamSynergy
+{
+    public class AnomalyDetection
+    {
+        private double _threshold;
+
+        public AnomalyDetection(double threshold = 0.8)
+        {
+            _threshold = threshold;
+        }
+
+        // Method to calculate anomaly based on similarity threshold
+        public bool IsAnomaly(double predictedValue, double actualValue)
+        {
+            return Math.Abs(predictedValue - actualValue) > _threshold;
+        }
+
+    }
+}
