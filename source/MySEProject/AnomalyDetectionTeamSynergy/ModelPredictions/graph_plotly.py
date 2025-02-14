490d6f9787677fccfd24f09c15b37950579bbc5a
Muhammad-Talha-S <talhasheikh878@gmail.com>
2025-02-14 12:30:24 +0100

    create fig graph objects

--- source/MySEProject/AnomalyDetectionTeamSynergy/ModelPredictions/graph_plotly.py
+++ source/MySEProject/AnomalyDetectionTeamSynergy/ModelPredictions/graph_plotly.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import plotly.graph_objects as go
 
-df = pd.read_csv("sequence_1_predictions.csv")
+sequence_df = pd.read_csv("sequence_1_predictions.csv")
+sequence_df.replace("-", None, inplace=True)
+sequence_df = sequence_df.apply(pd.to_numeric, errors='coerce')
 
-df.replace("-", None, inplace=True)
-df = df.apply(pd.to_numeric, errors='coerce')
-
-
-actual_values = df["Actual"]
-predicted_values = df["Predicted"]
-
+actual_values = sequence_df["Actual"]
+predicted_values = sequence_df["Predicted"]
 x_axis = list(range(1, len(actual_values) + 1))
 
-print(x_axis)
-print(actual_values)
-print(predicted_values)
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(x=x_axis, y=actual_values, mode='lines+markers', name='Actual Values'))
+fig.add_trace(go.Scatter(x=x_axis, y=predicted_values, mode='lines+markers', name='Predicted Values'))
+
+# Customize layout
+fig.update_layout(title='Actual vs Predicted Values',
+                  xaxis_title='Sequence Number',
+                  yaxis_title='Values')
+
+# Show the plot
+fig.show()
