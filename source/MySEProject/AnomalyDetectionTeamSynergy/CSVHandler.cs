1663c35b636b9d905a7bae65d93440008b8bc754
Waqas Ahmed <waqasahmed.ht@gmail.com>
2025-02-18 19:19:17 +0100

    adding documentation in code

--- source/MySEProject/AnomalyDetectionTeamSynergy/CSVHandler.cs
+++ source/MySEProject/AnomalyDetectionTeamSynergy/CSVHandler.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
+﻿using System.Globalization;
 
 namespace AnomalyDetectionTeamSynergy
 {
     /// <summary>
-    /// Provides functionality to read and parse sequences from a CSV file.
+    /// Provides functionality to read, parse, manipulate, and save sequences from/to CSV files.
+    /// This class is designed to handle numerical data sequences, ensuring they are correctly formatted and processed.
     /// </summary>
     public class CSVHandler
     {
         /// <summary>
-        /// Parses numerical sequences from a CSV file.
+        /// Parses numerical sequences from a CSV file. Each line in the CSV file represents a sequence of double values.
+        /// The first line is assumed to be a header and is skipped. Sequences with fewer than 3 values are ignored.
         /// </summary>
-        /// <param name="filePath">The path to the CSV file.</param>
-        /// <returns>A list of sequences, where each sequence is a list of double values.</returns>
+        /// <param name="filePath">The path to the CSV file containing the sequences.</param>
+        /// <returns>A list of sequences, where each sequence is represented as a list of double values.</returns>
+        /// <exception cref="FileNotFoundException">Thrown if the specified file path does not exist.</exception>
+        /// <exception cref="FormatException">Thrown if any value in the CSV cannot be parsed as a double.</exception>
         public List<List<double>> ParseSequencesFromCSV(string filePath)
         {
             // Initialize a list to store sequences
@@ -65,10 +65,13 @@ namespace AnomalyDetectionTeamSynergy
 
         /// <summary>
         /// Trims the first N elements from each sequence in a list of sequences.
+        /// This method is useful for removing initial data points that may not be relevant for analysis.
         /// </summary>
-        /// <param name="sequences">A list of lists, where each inner list represents a sequence of double values.</param>
+        /// <param name="sequences">A list of sequences, where each sequence is a list of double values.</param>
         /// <param name="N">The number of elements to remove from the beginning of each sequence.</param>
-        /// <returns>A new list of lists where each inner list has its first N elements removed.</returns>
+        /// <returns>A new list of sequences with the first N elements removed from each sequence.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if the sequences parameter is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if N is greater than the length of any sequence.</exception>
         public List<List<double>> TrimSequences(List<List<double>> sequences, int N)
         {
             List<List<double>> trimmedSequences = new List<List<double>>();
@@ -84,8 +87,10 @@ namespace AnomalyDetectionTeamSynergy
 
         /// <summary>
         /// Displays the parsed sequence data in the console.
+        /// This method is primarily used for debugging and verification purposes.
         /// </summary>
-        /// <param name="sequences">A list of numerical sequences to display.</param>
+        /// <param name="sequences">A list of sequences to display, where each sequence is a list of double values.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the sequences parameter is null.</exception>
         public void DisplaySequenceData(List<List<double>> sequences)
         {
             Console.WriteLine("Displaying sequence data:");
@@ -95,6 +100,16 @@ namespace AnomalyDetectionTeamSynergy
             }
         }
 
+        /// <summary>
+        /// Saves a sequence and its corresponding predicted sequence to a CSV file.
+        /// The file is saved in a specified directory within the project, and the directory is created if it does not exist.
+        /// </summary>
+        /// <param name="fileName">The name of the CSV file to save the data to.</param>
+        /// <param name="sequence">The actual sequence of double values.</param>
+        /// <param name="predictedSequence">The predicted sequence of string values.</param>
+        /// <exception cref="ArgumentException">Thrown if the sequence and predictedSequence lists are not of equal size.</exception>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the specified directory path is invalid.</exception>
+        /// <exception cref="IOException">Thrown if an I/O error occurs while writing to the file.</exception>
         public void SaveToCsv(string fileName, List<double> sequence, List<string> predictedSequence)
         {
             string projectbaseDirectory = Directory.GetParent(Directory.GetCurrentDirectory())!.Parent!.Parent!.FullName;
