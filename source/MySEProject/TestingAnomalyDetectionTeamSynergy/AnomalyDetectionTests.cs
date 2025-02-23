09fc0eff7771f9e6c411a33a5cc71e0c42d01821
Muhammad-Talha-S <talhasheikh878@gmail.com>
2025-02-23 16:18:25 +0100

    extend AnomalyDetection test

--- source/MySEProject/TestingAnomalyDetectionTeamSynergy/AnomalyDetectionTests.cs
+++ source/MySEProject/TestingAnomalyDetectionTeamSynergy/AnomalyDetectionTests.cs
@@ -16,5 +16,18 @@ namespace TestingAnomalyDetectionTeamSynergy
 
             Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        public void Test_IsAnomaly_WithoutAnomaly()
+        {
+            var anomalyDetection = new AnomalyDetection();
+            double predictedValue = 100;
+            double actualValue = 105;
+            double absoluteThreshold = 1;
+            double relativeThreshold = 0.1;
+            bool result = anomalyDetection.IsAnomaly(predictedValue, actualValue, absoluteThreshold, relativeThreshold);
+
+            Assert.IsFalse(result);
+        }
     }
 }
