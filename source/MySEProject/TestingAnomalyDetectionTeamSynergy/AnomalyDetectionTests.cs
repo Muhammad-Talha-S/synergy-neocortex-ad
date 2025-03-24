729dc8daab662ac7daee52dd44e866ca2df29ccf
Muhammad-Talha-S <talhasheikh878@gmail.com>
2025-03-24 11:18:17 +0500

    adding documentation for tests

--- source/MySEProject/TestingAnomalyDetectionTeamSynergy/AnomalyDetectionTests.cs
+++ source/MySEProject/TestingAnomalyDetectionTeamSynergy/AnomalyDetectionTests.cs
@@ -1,9 +1,15 @@
 ﻿using AnomalyDetectionTeamSynergy;
 namespace TestingAnomalyDetectionTeamSynergy
 {
+    /// <summary>
+    /// Unit tests for AnomalyDetection class
+    /// </summary>
     [TestClass]
     public class AnomalyDetectionTests
     {
+        /// <summary>
+        /// Tests anomaly detection when deviation exceeds both thresholds
+        /// </summary>
         [TestMethod]
         public void Test_IsAnomaly_WithAnomaly()
         {
@@ -12,12 +18,15 @@ namespace TestingAnomalyDetectionTeamSynergy
             var anomalyDetection = new AnomalyDetection(relativeThreshold, absoluteThreshold);
             double predictedValue = 100;
             double actualValue = 120;
-            
+
             bool result = anomalyDetection.IsAnomaly(predictedValue, actualValue);
 
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Tests normal case where deviation is within thresholds
+        /// </summary>
         [TestMethod]
         public void Test_IsAnomaly_WithoutAnomaly()
         {
@@ -26,7 +35,7 @@ namespace TestingAnomalyDetectionTeamSynergy
             var anomalyDetection = new AnomalyDetection(relativeThreshold, absoluteThreshold);
             double predictedValue = 100;
             double actualValue = 105;
-            
+
             bool result = anomalyDetection.IsAnomaly(predictedValue, actualValue);
 
             Assert.IsFalse(result);
--- source/MySEProject/TestingAnomalyDetectionTeamSynergy/SequenceAnalyzerTests.cs
+++ source/MySEProject/TestingAnomalyDetectionTeamSynergy/SequenceAnalyzerTests.cs
@@ -4,53 +4,40 @@ using AnomalyDetectionTeamSynergy;
 
 namespace TestingAnomalyDetectionTeamSynergy
 {
+    /// <summary>
+    /// Tests for SequenceAnalyzer utility methods
+    /// </summary>
     [TestClass]
     public class SequenceAnalyzerTests
     {
+        /// <summary>
+        /// Tests calculation of minimum value difference between training and inference sequences
+        /// </summary>
         [TestMethod]
         public void Test_FindDifferenceBetweenTwoMinValues()
         {
             // Arrange
-            var trainingSequences = new List<List<double>>
-            {
-                new List<double> {5.0, 4.0 },
-                new List<double> { 1.0, 3.0 }
-            };
-            var inferringSequences = new List<List<double>>
-            {
-                new List<double> { 2.0, 3.0 },
-                new List<double> { 6.0, 4.0,}
-            };
+            var trainingSequences = new List<List<double>> { new List<double> { 5.0, 4.0 }, new List<double> { 1.0, 3.0 } };
+            var inferringSequences = new List<List<double>> { new List<double> { 2.0, 3.0 }, new List<double> { 6.0, 4.0 } };
             var analyzer = new SequenceAnalyzer(trainingSequences, inferringSequences);
 
-            // Act
-            var difference = analyzer.FindDifferenceBetweenTwoMinValues();
-
-            // Assert
-            Assert.AreEqual(1.0, difference);
+            // Act & Assert
+            Assert.AreEqual(1.0, analyzer.FindDifferenceBetweenTwoMinValues());
         }
 
+        /// <summary>
+        /// Tests finding maximum value across all sequences
+        /// </summary>
         [TestMethod]
         public void Test_FindMaxValue()
         {
             // Arrange
-            var trainingSequences = new List<List<double>>
-            {
-                new List<double> { -1.5, 2.3, 4.7 },
-                new List<double> { 0.0, -3.2, 5.1 }
-            };
-            var inferringSequences = new List<List<double>>
-            {
-                new List<double> { -2.0, 3.8, 1.2 },
-                new List<double> { 6.0, -4.5, 0.1 }
-            };
+            var trainingSequences = new List<List<double>> { new List<double> { -1.5, 2.3, 4.7 }, new List<double> { 0.0, -3.2, 5.1 } };
+            var inferringSequences = new List<List<double>> { new List<double> { -2.0, 3.8, 1.2 }, new List<double> { 6.0, -4.5, 0.1 } };
             var analyzer = new SequenceAnalyzer(trainingSequences, inferringSequences);
 
-            // Act
-            var maxValue = analyzer.FindMaxValue();
-
-            // Assert
-            Assert.AreEqual(6.0, maxValue);
+            // Act & Assert
+            Assert.AreEqual(6.0, analyzer.FindMaxValue());
         }
     }
 }
